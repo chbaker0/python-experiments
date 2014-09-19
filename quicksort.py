886af75390ba052b197a04a8a6b658cad5bcc488
Collin Baker <chbaker0@gmail.com>
2014-09-19 15:56:05 -0400

    Changed pivot position
    Signed-off-by: Collin Baker <chbaker0@gmail.com>

--- quicksort.py
+++ quicksort.py
@@ -3,20 +3,21 @@ def quicksort_pivot_select(l, b, e):
 
 def quicksort_partition(l, b, e):
     pivot_index = quicksort_pivot_select(l, b, e)
-    l[pivot_index], l[b] = l[b], l[pivot_index]
+    l[pivot_index], l[e-1] = l[e-1], l[pivot_index]
 
     left_div = 1
-    right_div = e
+    right_div = e-1
 
     while left_div < right_div:
-        if l[left_div] <= l[b]:
+        if l[left_div] < l[e-1]:
             left_div += 1
         else:
             right_div -= 1
             l[left_div], l[right_div] = l[right_div], l[left_div]
 
-    l[b], l[left_div-1] = l[left_div-1], l[b]
-    return left_div-1
+    l[left_div], l[e-1] = l[e-1], l[left_div]
+
+    return left_div
         
 def quicksort_impl(l, b, e):
     if e - b <= 1:
